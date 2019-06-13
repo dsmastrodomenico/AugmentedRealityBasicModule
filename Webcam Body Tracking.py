14c1697d0365634ccb8cc6d4a3d33689ea60aa7e
dsmastrodomenico <darwin.mastrodomenico@gmail.com>
2019-06-13 03:47:18 -0500

    Tracking Algorithm

---
+++ Webcam Body Tracking.py
@@ -0,0 +1,122 @@
+import sys
+import cv2 as cv 
+import numpy as np 
+
+class BodyTrackerSystem(): 
+    def __init__(self):
+        # Captura el marco de entrada 
+        self.cap = cv.VideoCapture(0) 
+
+        # Factor de submuestreo para la imagen. 
+        self.scaling_factor = 1 
+
+        # Numero de cuadros para mantener en el bufer cuando realiza el seguimiento. Si aumenta este numero, los puntos de caracteristica tendran mas "inercia"
+        self.num_frames_to_track = 5 
+
+        # Saltar cada 'n' cuadros. Esto es solo para aumentar la velocidad.
+        self.num_frames_jump = 2 
+
+        # 'winSize' se refiere al tamano de cada parche. Estos parches son los bloques mas pequenos en los que operamos y rastreamos los puntos de caracteristicas.
+        self.tracking_params = dict(winSize = (11, 11), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03)) 
+
+    def compute_feature_points(self, tracking_paths, prev_img, current_img):
+        feature_points = [tp[-1] for tp in tracking_paths]
+        
+        # Vector de puntos 2D para los cuales es necesario encontrar el flujo.
+        feature_points_0 = np.float32(feature_points).reshape(-1, 1, 2) 
+
+        feature_points_1, status_1, err_1 = cv.calcOpticalFlowPyrLK(prev_img, 
+            current_img, feature_points_0, None, **self.tracking_params) 
+        feature_points_0_rev, status_2, err_2 = cv.calcOpticalFlowPyrLK(current_img,
+            prev_img, feature_points_1, None, **self.tracking_params)
+
+        # Calcular la diferencia de los puntos de caracteristicas 
+        diff_feature_points = abs(feature_points_0-feature_points_0_rev).reshape(-1, 2).max(-1) 
+
+        # Umbral y mantener solo los puntos buenos. 
+        good_points = diff_feature_points < 1
+        return feature_points_1.reshape(-1, 2), good_points
+
+        # Extraer area de interes basada en las rutas de seguimiento (tracking_paths)
+
+    # En caso de que no haya ninguno, se utiliza todo el cuadro.
+    def calculate_region_of_interest(self, frame, tracking_paths):
+        mask = np.zeros_like(frame) 
+        mask[:] = 255 
+        for x, y in [np.int32(tp[-1]) for tp in tracking_paths]: 
+            cv.circle(mask, (x, y), 6, 0, -1) 
+        return mask
+
+    def add_tracking_paths(self, frame, tracking_paths):
+        mask = self.calculate_region_of_interest(frame, tracking_paths)
+
+        # Extrae buenas caracteristicas para seguir. 
+        feature_points = cv.goodFeaturesToTrack(frame, mask = mask, maxCorners = 500, \
+            qualityLevel = 0.3, minDistance = 7, blockSize = 7) 
+
+        if feature_points is not None: 
+            for x, y in np.float32(feature_points).reshape(-1, 2): 
+                tracking_paths.append([(x, y)])
+
+    def start_tracking(self): 
+        tracking_paths = [] 
+        frame_index = 0 
+
+        # Iterar hasta que el usuario presione la tecla ESC. 
+        while True: 
+            # leer el marco de entrada 
+            ret, frame = self.cap.read() 
+
+            # bajar la muestra del marco de entrada 
+            frame = cv.resize(frame, None, fx=self.scaling_factor, fy=self.scaling_factor,
+                interpolation=cv.INTER_AREA) 
+
+            frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) 
+            output_img = frame.copy() 
+            
+
+            if len(tracking_paths) > 0: 
+                prev_img, current_img = prev_gray, frame_gray
+                # Calcular puntos de caracteristicas utilizando flujo optico. 
+                
+                feature_points, good_points = self.compute_feature_points(tracking_paths,
+                    prev_img, current_img)
+
+                new_tracking_paths = []
+                for tp, (x, y), good_points_flag in zip(tracking_paths, feature_points, good_points): 
+                    if not good_points_flag: continue 
+
+                    tp.append((x, y)) 
+
+                    # Usando la estructura de cola, primero en entrar, primero en salir
+                    if len(tp) > self.num_frames_to_track: del tp[0] 
+
+                    new_tracking_paths.append(tp) 
+
+                    # dibujar circulos verdes en la parte superior de la imagen de salida 
+                    cv.circle(output_img, (x, y), 3, (0, 255, 0), -1) 
+
+                tracking_paths = new_tracking_paths 
+
+                # dibujar lineas verdes en la parte superior de la imagen de salida 
+                point_paths = [np.int32(tp) for tp in tracking_paths]
+                cv.polylines(output_img, point_paths, False, (0, 150, 0)) 
+
+            # 'if' condicion para saltar cada 'n' cuadros 
+            if not frame_index % self.num_frames_jump: 
+                self.add_tracking_paths(frame_gray, tracking_paths)
+
+            frame_index += 1 
+            prev_gray = frame_gray 
+
+            cv.imshow('Flujo Optico', output_img) 
+
+            # Compruebe si el usuario ha pulsado la tecla ESC.
+            c = cv.waitKey(1) 
+            if c == 27: 
+                break
+        cv.destroyAllWindows()
+
+if __name__ == '__main__': 
+    BodyTrackerSystem().start_tracking() 
+    
---
+++ Webcam Object Tracking.py
@@ -0,0 +1,108 @@
+import sys 
+import cv2 as cv
+import numpy as np 
+
+class ObjectTrackerSystem(): 
+    def __init__(self): 
+        # Inicializar el objeto de captura de video.
+        # 0 -> indica que el cuadro debe ser capturado de la camara
+        self.cap = cv.VideoCapture(0) 
+
+        # Captura el cuadro desde la camara 
+        ret, self.frame = self.cap.read() 
+
+        # Factor de submuestreo para el cuadro de entrada
+        self.scaling_factor = 1 
+        self.frame = cv.resize(self.frame, None, fx=self.scaling_factor, fy=self.scaling_factor, interpolation=cv.INTER_AREA) 
+
+        cv.namedWindow('Tracking de Objetos') 
+        cv.setMouseCallback('Tracking de Objetos', self.mouse_event) 
+
+        self.selection = None 
+        self.drag_start = None 
+        self.tracking_state = 0 
+
+    # Metodo para rastrear eventos del mouse
+    def mouse_event(self, event, x, y, flags, param): 
+        x, y = np.int16([x, y]) 
+
+        # Detectar el evento del movimiento del boton del mouse hacia abajo
+        if event == cv.EVENT_LBUTTONDOWN: 
+            self.drag_start = (x, y) 
+            self.tracking_state = 0 
+
+        if self.drag_start:
+            if event == cv.EVENT_MOUSEMOVE:
+                h, w = self.frame.shape[:2] 
+                xo, yo = self.drag_start 
+                x0, y0 = np.maximum(0, np.minimum([xo, yo], [x, y])) 
+                x1, y1 = np.minimum([w, h], np.maximum([xo, yo], [x, y])) 
+                self.selection = None 
+
+                if x1-x0 > 0 and y1-y0 > 0:
+                    self.selection = (x0, y0, x1, y1) 
+
+            elif event == cv.EVENT_LBUTTONUP:
+                self.drag_start = None 
+                if self.selection is not None: 
+                    self.tracking_state = 1 
+
+    # Definicion del metodo para iniciar el seguimiento del objeto.
+    def start_tracking(self): 
+        # Seccion para repetir hasta que el usuario presione la tecla Esc. 
+        while True: 
+            # Captura el cuadro desde la camara
+            ret, self.frame = self.cap.read() 
+            # Cambiar el tamano del marco de entrada
+            self.frame = cv.resize(self.frame, None, fx=self.scaling_factor, fy=self.scaling_factor, interpolation=cv.INTER_AREA) 
+
+            vis = self.frame.copy() 
+
+            # Convercion de color HSV
+            hsv = cv.cvtColor(self.frame, cv.COLOR_BGR2HSV) 
+
+            # Crea una mascara basada en umbrales predefinidos.
+            mask = cv.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.))) 
+
+            if self.selection: 
+                x0, y0, x1, y1 = self.selection 
+                self.track_window = (x0, y0, x1-x0, y1-y0) 
+                hsv_roi = hsv[y0:y1, x0:x1] 
+                mask_roi = mask[y0:y1, x0:x1] 
+
+                # Calcular el histograma 
+                hist = cv.calcHist( [hsv_roi], [0], mask_roi, [16], [0, 180] ) 
+
+                # Normalizar y remodelar el histograma. 
+                cv.normalize(hist, hist, 0, 255, cv.NORM_MINMAX); 
+                self.hist = hist.reshape(-1) 
+
+                vis_roi = vis[y0:y1, x0:x1] 
+                cv.bitwise_not(vis_roi, vis_roi) 
+                vis[mask == 0] = 0 
+
+            if self.tracking_state == 1:
+                self.selection = None 
+
+                # Calcular la proyeccion posterior del histograma.
+                prob = cv.calcBackProject([hsv], [0], self.hist, [0, 180], 1) 
+
+                prob &= mask 
+                term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 ) 
+
+                # Aplicar CAMShift en 'prob' 
+                track_box, self.track_window = cv.CamShift(prob, self.track_window, term_crit) 
+
+                # Dibuja una elipse alrededor del objeto. 
+                cv.ellipse(vis, track_box, (0, 255, 0), 2) 
+
+            cv.imshow('Tracking de Objetos', vis) 
+
+            c = cv.waitKey(delay=5) 
+            if c == 27: 
+                break 
+
+        cv.destroyAllWindows() 
+
+if __name__ == '__main__': 
+    ObjectTrackerSystem().start_tracking()
